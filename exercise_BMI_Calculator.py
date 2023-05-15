deeabf9b7fb0f2eb9a8e66c95917779f802c5c39
mizar <mizarzulfaz@gmail.com>
2023-05-15 19:06:22 +0700

    initial

---
+++ exercise_BMI_Calculator.py
@@ -0,0 +1,11 @@
+
+weight = input("enter your weight in kg: ")
+height = input("enter your height in m: ")
+
+weight = int(weight)
+height = float(height)
+
+BMI = weight / pow(height, 2)
+
+
+print(int(BMI))
