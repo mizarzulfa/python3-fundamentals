97a12a2713c28aabe48b64f147c52e3ecdb62887
mizar <mizarzulfaz@gmail.com>
2023-05-17 21:10:49 +0700

    Displaying Variables with F-Strings

---
+++ 3_Displaying_Variables_with_F-Strings.py
@@ -0,0 +1,22 @@
+score = 0
+height = 1.9
+isWinning = True
+
+print(
+    f"Your score is {score}, Your height is {height}, You're winning right? {isWinning}")
+
+
+'''
+In the code snippet, \
+I initialized three variables: score, height, and isWinning.
+The score variable is set to 0, representing a player's score in a game.
+The height variable is assigned a value of 1.9, representing the player's height in meters.
+Finally, the isWinning variable is set to True, indicating that the player is currently winning.
+
+To display the values of these variables, I used a formatted string using the print function.
+The string includes placeholders that are replaced with the values of the variables.
+The output of this code snippet would be:
+
+"Your score is 0, Your height is 1.9, You're winning right? True
+
+'''
