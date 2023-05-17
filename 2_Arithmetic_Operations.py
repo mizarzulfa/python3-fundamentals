d591080f6dfb7033a758b86c36d25b3a07e9a678
mizar <mizarzulfaz@gmail.com>
2023-05-17 20:56:34 +0700

    Arithmetic

---
+++ 2_Arithmetic_Operations.py
@@ -0,0 +1,32 @@
+# Initialize the variable
+easy_peasy = 90
+
+# Perform arithmetic operations
+easy_peasy /= 10  # Divide by 10
+easy_peasy += 5   # Add 5
+easy_peasy *= 5   # Multiply by 5
+easy_peasy /= 4.2  # Divide by 4.2
+
+# Print the result rounded to 2 decimal places
+print(round(easy_peasy, 2))
+
+
+'''
+This code demonstrates basic arithmetic operations in Python.
+It performs a series of calculations on the variable easy_peasy and prints the result.
+
+^ Explanation ^
+- The variable easy_peasy is initialized with a value of 90.
+- The /= operator performs division and assignment in one step. easy_peasy /= 10 divides easy_peasy by 10 and updates its value to the result (9.0).
+- The += operator adds the right operand to the left operand and assigns the result to the left operand. easy_peasy += 5 adds 5 to easy_peasy and updates its value to 14.0.
+- The *= operator multiplies the right operand with the left operand and assigns the result to the left operand. easy_peasy *= 5 multiplies easy_peasy by 5 and updates its value to 70.0.
+- The /= operator is used again to divide easy_peasy by 4.2 and update its value to 16.666666666666668.
+- The round() function is used to round the value of easy_peasy to 2 decimal places.
+- The rounded value of easy_peasy (16.67) is printed to the console
+
+Learning Focus
+Assignment operators (/=, +=, *=) for performing operations and updating variables in a single step.
+Arithmetic operations in Python, such as division and multiplication.
+Rounding numbers using the round() function.
+
+'''
