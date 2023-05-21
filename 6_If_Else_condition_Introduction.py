2b8ddcc4a543b1f39e99fa58fa937723aa5bbccb
mizar <mizarzulfaz@gmail.com>
2023-05-21 21:29:18 +0700

    docs update

--- 6_If_Else_condition_Introduction.py
+++ 6_If_Else_condition_Introduction.py
@@ -14,4 +14,24 @@ If height is greater than or equals to 120, the statement under the first 'if' b
 If it is not, the condition in the first 'elif' block is evaluated.
 If that condition is also False, the code falls back to the else block.
 
+Note:
+ I used to code in C#, and the formatting of if/else conditions differs between C# and Python.
+ Here's a comparison of the syntax in both languages:
+
+In C#, the syntax for an if/else condition looks like this:
+ if (condition)
+     { // Code to execute if the condition is true }
+  else
+    { // Code to execute if the condition is false }
+    
+On the other hand, in Python, the syntax is as follows:
+if condition:
+    # Code to execute if the condition is true
+else:
+    # Code to execute if the condition is false
+
+in C#, the if and else blocks are enclosed within curly braces {}, whereas in Python,
+indentation is used to define the blocks of code.
+This difference in formatting is important to keep in mind when transitioning between the two languages.
+
 '''
