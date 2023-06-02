975a5cdc3c8a277681e258232a7f94dbdf8ababf
mizar <mizarzulfaz@gmail.com>
2023-06-02 19:23:45 +0700

    Desc added

--- 2_2_Treasure_Island.py
+++ 2_2_Treasure_Island.py
@@ -47,3 +47,38 @@ elif crossroad == "right":
     print("Game Over.")
 else:
     print("Error, Please input the correct words!!")
+
+print('''
+---------------
+Description: In this Python program, you embark on an exciting adventure to find the hidden treasure on Treasure Island. 
+You will make choices at various points in the game that will determine your fate.
+The program utilizes user input and conditional statements to guide you through the game and determine 
+whether you succeed or fail.
+
+Learning Focus:
+This program provides a beginner-friendly introduction to using user input and conditional statements in Python.
+By going through the code and understanding how different choices lead to different outcomes,
+you can learn about basic flow control and decision-making in programming.
+
+Code Explanation:
+- The program starts with a multiline string that represents a visually appealing ASCII art of a chest.
+   It serves as a decorative element to enhance the game's atmosphere.
+- The `print` function is used to display a welcome message to the player.
+- The `input` function is used to prompt the player for their choice at the first crossroad, which is stored in the variable `crossroad`.
+- The `lower` method is applied to the `crossroad` variable to convert the input to lowercase for case-insensitive comparison.
+- The program uses conditional statements (`if`, `elif`, and `else`) to handle the different outcomes based on the player's choices.
+- If the player chooses to go "left" at the crossroad, they are prompted for another choice at the lake. The input is stored in the variable `lake`.
+- Similar to the previous choice, the input is converted to lowercase for comparison.
+- Depending on the choice at the lake, the program guides the player through different scenarios using nested conditional statements.
+- If the player chooses to wait ("wait"), they are asked to choose a door at the island. Their choice is stored in the variable `doors`.
+- Again, the input is converted to lowercase for comparison.
+- Based on the door choice, the program determines whether the player has found the treasure or encountered a game-over situation.
+- If the player chooses to swim ("swim") at the lake, it results in a game-over.
+- If the player chooses to go "right" at the crossroad, it also leads to a game-over.
+- If the player enters an invalid input, an error message is displayed.
+- The program uses appropriate `print` statements to communicate the outcomes to the player.
+
+Note: This program can be further enhanced by implementing loops and additional choices
+to make the gameplay more complex and engaging.
+
+''')
