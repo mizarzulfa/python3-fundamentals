bdfc1f569151b7ca29a2529177ce92f49bcce0dc
mizar <mizarzulfaz@gmail.com>
2023-06-14 20:42:41 +0700

    For LOOPS Introduction

---
+++ 3_1_FOR_LOOPS_Introduction.py
@@ -0,0 +1,36 @@
+# Introduction
+
+# List data
+vegetables = ["carrot", "broccoli", "tomato", "spinach", "cabbage"]
+
+# FOR LOOPS
+for var_loops in vegetables:
+    print(var_loops)
+    print(var_loops + " bro")
+
+
+print('''
+---------------
+Looping and String Concatenation with Vegetables
+
+Description:
+In this Python code, you will learn how to use a loop to iterate over a list of vegetables and perform some actions with each item.
+The code will print the vegetable's name and then concatenate it with the word "bro" to create a new string.
+This exercise will help to understand the basics of loops, string manipulation, and list iteration in Python.
+
+Learning Focus:
+- Looping over a list using a for loop
+- Printing values within a loop
+- Concatenating strings
+
+Code Explanation:
+1. The variable `vegetables` is initialized as a list containing the names of various vegetables.
+2. The `for` loop is used to iterate over each item in the `vegetables` list.
+3. Inside the loop, the current vegetable's name is printed using the `print` function, which displays the value on the screen.
+4. The `+` operator is used for string concatenation. By adding the string " bro" to each vegetable's name, a new string is created.
+5. The concatenated string is printed using the `print` function.
+6. The loop continues until all the vegetables in the list have been processed.
+
+By running this code, you will see each vegetable's name followed by the concatenated string, which appends " bro" to each vegetable's name.
+This exercise demonstrates how to perform actions on each item in a list using a loop, and how to manipulate strings in Python.
+''')
