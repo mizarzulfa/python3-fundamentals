1cd9546754fd79a19a841d12291057183e9bfcdb
mizar <mizarzulfaz@gmail.com>
2023-06-20 21:17:08 +0700

    Built-in Function added

--- 3_3_0_For_Loops_Max_numbers.py
+++ 3_3_0_For_Loops_Max_numbers.py
@@ -6,10 +6,14 @@
 student_score = input("Input a list of student scores: ").split()
 # string to int ( list )
 student_score = [int(k) for k in student_score]
-max_numbers = 0
 
+max_numbers = 0
 for z in student_score:
     if z > max_numbers:
         max_numbers = z
 
+# for loop methods, checking each numbers
 print(f"The highest score in the class is: {max_numbers}")
+
+# Using Built-in Python Function
+print(f"The highest score in the class is: {max(student_score)}")
