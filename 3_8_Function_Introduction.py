a7ff7b7ab8c2efa36276ac7b875a8857f8d1f8f8
mizar <mizarzulfaz@gmail.com>
2023-06-26 20:44:39 +0700

    Python Function and Calling

---
+++ 3_8_Function_Introduction.py
@@ -0,0 +1,10 @@
+# Functions Introductions
+
+def variable_z():
+    print("Hello")
+    print("Goodbye!")
+# Nothing Happen until
+
+
+# We call Function
+variable_z()  # for this case: there's no input inside brackets required
