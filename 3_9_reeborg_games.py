1d5715dc2affa10358abae54861de06303c3107b
mizar <mizarzulfaz@gmail.com>
2023-06-26 21:47:03 +0700

    while loop 1

---
+++ 3_9_reeborg_games.py
@@ -0,0 +1,24 @@
+# SAMPLE ONLY
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def anjay():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+counter = 6
+while counter > 0:
+    anjay()
+    counter -= 1
