5d5fc44952747f8378877395a8a9c06eaeb76f2f
mizar <mizarzulfaz@gmail.com>
2023-07-14 20:10:58 +0700

    Loop until "_" filled

--- 4_1_Hangman_project.py
+++ 4_1_Hangman_project.py
@@ -1,28 +1,33 @@
-# 2
 import random
-word_list = ["aardvark", "baboon", "camel"]
 
+
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(f"hint for testing: {chosen_word}")
 
-guess = input("Guess a letter: ").lower()
-
 new_words = []
 
-for check in chosen_word:
-    # print(check)
-    if check == guess:
-        new_words.append(check)
-        # print("right")
-    else:
-        new_words.append("_")
+for i in range(len(chosen_word)):
+    new_words += "_"
+
+while "_" in new_words:
+    guess = input("Guess a letter: ").lower()
+
+    for check in range(len(chosen_word)):
+        string_check = chosen_word[check]
+        if string_check == guess:
+            new_words[check] = string_check
+
+    print(new_words)
+
+print("You Win.")
 
 # Convert list to string using for loop method
-    # words_combine = ""
-    # for i in new_words:
-    #     words_combine = words_combine + i
-    # print(words_combine)
+# words_combine = ""
+# for i in new_words:
+#     words_combine = words_combine + i
+# print(words_combine)
 
 # Convert list to string using join function
-res = ' '.join(new_words)
-print(res)
+# res = ' '.join(new_words)
+# print(res)
