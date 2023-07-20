d1e152ef2fd075a87424bce4249923e9921431f1
mizar <mizarzulfaz@gmail.com>
2023-07-20 19:42:04 +0700

    While Loop Exit

--- 4_1_Hangman_project.py
+++ 4_1_Hangman_project.py
@@ -1,6 +1,5 @@
 import random
 
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(f"hint for testing: {chosen_word}")
@@ -10,7 +9,8 @@ new_words = []
 for i in range(len(chosen_word)):
     new_words += "_"
 
-while "_" in new_words:
+underscore_remain = False
+while underscore_remain == False:  # alt : while not underscore_remain:
     guess = input("Guess a letter: ").lower()
 
     for check in range(len(chosen_word)):
@@ -20,7 +20,22 @@ while "_" in new_words:
 
     print(new_words)
 
-print("You Win.")
+    if "_" not in new_words:
+        print("You win.")
+        underscore_remain = True
+
+## FIRST METHOD ##
+# while "_" in new_words:
+#     guess = input("Guess a letter: ").lower()
+
+#     for check in range(len(chosen_word)):
+#         string_check = chosen_word[check]
+#         if string_check == guess:
+#             new_words[check] = string_check
+
+#     print(new_words)
+
+# print("You Win.")
 
 # Convert list to string using for loop method
 # words_combine = ""
