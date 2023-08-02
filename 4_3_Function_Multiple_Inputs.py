6ef166030b4bbbde96f8bbe7c1f722e1d5b92e1f
mizar <mizarzulfaz@gmail.com>
2023-08-02 19:47:36 +0700

    Return Value

--- 4_3_Function_Multiple_Inputs.py
+++ 4_3_Function_Multiple_Inputs.py
@@ -1,9 +1,17 @@
-def greet_with(name: str,location: str):
+# def greet_with(name: str,location: str):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}")
+    
+# # # POSITIONAL ARGUMENTS
+# greet_with("Anjay","Indonesia")
+
+# # # KEYWORD ARGUMENTS
+# greet_with(location="Indonesia", name="Bodir")
+
+# # # Multiple Parameters & Returning Function VALUE
+def greet_with(name: str, location: str) -> str:
     print(f"Hello {name}")
     print(f"What is it like in {location}")
-    
-# # POSITIONAL ARGUMENTS
-greet_with("Anjay","Indonesia")
+    return f"{name} {location}"
 
-# # KEYWORD ARGUMENTS
-greet_with(location="Indonesia", name="Bodir")
+print(greet_with("anjay", "Unjuy"))
