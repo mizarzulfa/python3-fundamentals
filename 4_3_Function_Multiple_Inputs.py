20ef2eff46dbecfb2637def64308902654b1a337
mizar <mizarzulfaz@gmail.com>
2023-08-02 19:29:32 +0700

    Keyword Arguments

--- 4_3_Function_Multiple_Inputs.py
+++ 4_3_Function_Multiple_Inputs.py
@@ -2,4 +2,8 @@ def greet_with(name: str,location: str):
     print(f"Hello {name}")
     print(f"What is it like in {location}")
     
+# # POSITIONAL ARGUMENTS
 greet_with("Anjay","Indonesia")
+
+# # KEYWORD ARGUMENTS
+greet_with(location="Indonesia", name="Bodir")
