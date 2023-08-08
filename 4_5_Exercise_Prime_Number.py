27d5dca273ca1e835492e95ad3e32adfae7df9e9
mizar <mizarzulfaz@gmail.com>
2023-08-08 20:56:11 +0700

    some changes

--- 4_5_Exercise_Prime_Number.py
+++ 4_5_Exercise_Prime_Number.py
@@ -1,5 +1,5 @@
 
-# #NO LOOP
+# #NO LOOP V1
 # def prime_checker(number):
 #     if number < 2:
 #         print("Not a prime number")
@@ -16,28 +16,46 @@
 # prime_checker(number=n)
 
 
-
-# LOOP
-
+# NO LOOP V2
 def prime_checker(number):
+    is_prime = True
     if number < 2:
-        print("Not a prime number")
         return
-
-    for i in range(2, int(number ** 0.5) + 1):
+    
+    for i in range(2, number):
         if number % i == 0:
-            print("Not a prime number")
-            return
+            is_prime = False
+            
+    if is_prime:
+        print("Prime")
+    else:
+        print("not prime")
         
-    print("Prime number")
+n = int(input("Check this number: "))
+prime_checker(number=n)
 
 
-while True:        
+# # LOOP
 
-    n = input("Check this number: ")
+# def prime_checker(number):
+#     if number < 2:
+#         print("Not a prime number")
+#         return
+
+#     for i in range(2, int(number ** 0.5) + 1):
+#         if number % i == 0:
+#             print("Not a prime number")
+#             return
+        
+#     print("Prime number")
+
+
+# while True:        
+
+#     n = input("Check this number: ")
     
-    if n.lower() == "c":
-        break
+#     if n.lower() == "c":
+#         break
     
-    numberss = int(n)
-    prime_checker(number=numberss)
+#     numberss = int(n)
+#     prime_checker(number=numberss)
