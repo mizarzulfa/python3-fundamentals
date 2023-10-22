01eba83218c8577565c834f70837dd28432015aa
mizar <mizarzulfaz@gmail.com>
2023-10-22 20:50:27 +0700

    python class 101

---
+++ 4_7_OWN_CLASS_101.py
@@ -0,0 +1,13 @@
+### PYTHON 3 Class 101
+
+class mantapbang:
+    def __init__(self, name : str, age : int):
+        self.name = name
+        self.age = age
+        
+
+cek = mantapbang("John", 101)
+
+print(cek.age)
+
+print(cek.name)
