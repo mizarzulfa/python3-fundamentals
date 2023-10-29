62718ae534d010a95b2d027b45a8d038a37b0e59
mizar <mizarzulfaz@gmail.com>
2023-10-29 21:14:14 +0700

    changes

--- zz_brain_recall.py
+++ zz_brain_recall.py
@@ -1,19 +1,35 @@
-def tambah(n1,n2):
-    a = n1 + n2
-    print(a)
+#### python class - add substract divide minus -- basic structure to understand
 
-
-class data:
-    def __init__(self) -> None:
-        self.anj = tambah(1,1)
-        self.unj = tambah(5,1)
+class calculator:
+    """
+    A simple calculator class with basic arithmetic operation
+    """
     
-    def pertambahan(self):
-        tambah(12,12)
+    def __init__(self, v1=10) -> None:
+    #    self.uhuy = v1
+       self.__uhuy = v1 ###protected members
     
-objek = data()
+    def add(self, n1, n2):
+        cek = n1 + self.__uhuy
+        return cek + n2
+    
+    def substract(self, n1, n2):
+        return n1 - n2
+    
+    def divide(self, n1, n2):
+        if n2 != 0:
+            return n1 / n2
+        else:
+            return "cannot divide by 0"
+        
+    def __str__(self) -> str:
+        return f'a calcualor with {self.__uhuy}'        
+        
+calc = calculator()
 
-has = objek.pertambahan()
+data = calc.add(2,2)
 
-objek.anj
-objek.unj
+sata = calc.divide(3,0)
+
+
+print(calc)
