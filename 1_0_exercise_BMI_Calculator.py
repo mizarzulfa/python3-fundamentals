8d0af20b4747cb3371baf762a18956813ba2b935
Mizar_Railway_server <mizarzulfaz@gmail.com>
2023-11-23 11:25:50 +0000

    update

--- 1_0_exercise_BMI_Calculator.py
+++ 1_0_exercise_BMI_Calculator.py
@@ -12,7 +12,7 @@ print(int(BMI))
 # The code calculates the Body Mass Index (BMI) based on the weight and height entered by the user.
 # Here's a step-by-step explanation:
 
-#anjay
+#mantappppp
 
 # 1. The user is prompted to enter their weight in kilograms using the `input()` function. The entered value is stored in the `weight` variable.
 # 2. The user is prompted to enter their height in meters using the `input()` function. The entered value is stored in the `height` variable.
